af2d1a6a1cfcfc21e4d5e646fb84c66f767fd960
zrottman <zrottman@example.com>
2023-08-05 11:05:24 -0700

    Initial implementation of queue

---
+++ queue.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+#include "node.h"
+
+Queue* new_queue(void)
+{
+    Queue *q;
+    q = malloc(sizeof(Queue));
+    q->head = NULL;
+    q->tail = NULL;
+    q->len  = 0;
+    return q;
+}
+
+int enqueue(Queue* q, Node *n)
+{
+    if (q->tail == NULL) {
+        q->head = q->tail = n;
+    } else {
+        q->tail->next = n;
+        q->tail = q->tail->next;
+    }
+    q->len++;
+    return q->len;
+}
+
+Node* dequeue(Queue* q, Node *n)
+{
+    Node *tmp;
+    if (q->head != NULL) {
+        tmp = q->head;
+        q->head = q->head->next;
+        q->len--;
+    }
+    return tmp;
+}
+
+void display_queue(Queue *q)
+{
+    Node *cur;
+    for(cur=q->head; cur != NULL; cur=cur->next)
+        printf("%d -> ", cur->value);
+    printf("\n");
+}
---
+++ queue.h
@@ -0,0 +1,16 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include "node.h"
+
+typedef struct Queue {
+    Node *head;
+    Node *tail;
+    int  len;
+} Queue;
+
+Queue*  new_queue(void);
+int     enqueue(Queue *q, Node *n);
+Node*   dequeue(Queue *q, Node *n);
+void    display_queue(Queue *q);
+
+#endif // QUEUE_H
