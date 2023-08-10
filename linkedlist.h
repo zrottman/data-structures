4d3d2e247a8dd0cec60d4d7e1f8a2ea4ea2d85b7
zrottman <zrottman@example.com>
2023-08-10 14:42:14 -0700

    TODO notes from Kevan review

--- linkedlist.h
+++ linkedlist.h
@@ -12,6 +12,9 @@ typedef struct LinkedList {
 LinkedList* CreateLinkedList(void);
 void        DestroyLinkedList(LinkedList *ll);
 size_t      ll_append(LinkedList *list, Node *new_node);
+// TODO: consider not exposing Node abstraction to user by
+// accepting key/value pair args instead ^
+//size_t      ll_append(LinkedList *list, char* key, char *val);
 void        ll_display(LinkedList *list);
 int         ll_delete_node(LinkedList *list, char* target);
 char*       ll_search(LinkedList *list, char* target);
