ca02ee21979cb92f9fd9d056114ea0ffacfe0ee6
zrottman <zrottman@example.com>
2023-09-29 14:07:14 -0700

    Revise to utilize array_display and cleanup

--- demos/array_demo.c
+++ demos/array_demo.c
@@ -3,24 +3,28 @@
 
 int main()
 {
-    Array *a = array_construct(8);
-
-    for (int i=0; i<100; ++i)
-        array_append(a, i);
-    
-    for (int i=0; i<100; ++i)
-        printf("%d ", a->items[i]);
-    printf("\n");
-
+    Array  *a = array_construct(8);
     size_t idx;
+
+    printf("Starting capacity: %zu\n", a->capacity);
+
+    // populate array
+    for (int i=0; i<100; ++i) { array_append(a, i); }
+
+    // display array
+    array_display(a);
+
+    // get
+    idx = 65;
+    printf("Getting idx %zu: %d\n", idx, array_get(a, idx));
     idx = 0;
     printf("Getting idx %zu: %d\n", idx, array_get(a, idx));
-    idx = 10;
-    printf("Getting idx %zu: %d\n", idx, array_get(a, idx));
-    idx = 12;
+    idx = 24;
     printf("Getting idx %zu: %d\n", idx, array_get(a, idx));
     idx = 99;
     printf("Getting idx %zu: %d\n", idx, array_get(a, idx));
+
+    printf("Ending capacity: %zu\n", a->capacity);
 
     array_destroy(&a);
 
