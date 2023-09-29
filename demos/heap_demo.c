6da839b921b50db8d6c3d3bfc54c6df590e3b117
zrottman <zrottman@example.com>
2023-09-29 14:28:40 -0700

    Simplify demo

--- demos/heap_demo.c
+++ demos/heap_demo.c
@@ -5,27 +5,31 @@ int main() {
     Heap* h = heap_construct();
 
     int  nums_count = 15;
-    int *nums;
+    int  *nums;
+    int  insert;
+    int  polled;
 
     // insert into heap
+    printf("Inserting into heap: ");
     for (int i=0; i<nums_count; ++i) { 
-        printf("Inserting %d\n", i+1);
-        heap_insert(h, i+1); 
-        heap_display(h);
+        insert = i+1;
+        printf("%d ", insert);
+        heap_insert(h, insert); 
     }
-
-    // display heap
-    printf("Displaying heap\n");
-    heap_display(h);
     printf("\n");
 
-    // poll
-    int polled;
+    // display heap
+    printf("Displaying underlying array: ");
+    heap_display(h);
 
+    // poll
+    printf("Polling from heap: ");
     for (int i=0; i<17; ++i) {
-        if (poll(h, &polled) == 0) { printf("Polling: %d\n", polled); }
-        heap_display(h);
+        if (poll(h, &polled) == 0) {
+            printf("%d ", polled); 
+        }
     }
+    printf("\n");
 
     heap_destroy(&h);
 
