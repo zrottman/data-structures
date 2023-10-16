cdd88740d5f3c747e5c0a231e758bb5886a88960
zrottman <zrottman@example.com>
2023-10-16 13:59:21 -0700

    Add tests

--- test/TestSort.c
+++ test/TestSort.c
@@ -15,20 +15,30 @@ void test_sort(void)
     int    solution[] = { 0, 1, 2, 3, 4, 8, 9 };
     size_t len        = 7;
 
-    merge_sort(&nums, len);
+    merge_sort(nums, len);
 
     for (int i=0; i<len; ++i) {
-        TEST_ASSSERT_EQUAL(solution[i], nums[i]);
+        TEST_ASSERT_EQUAL(solution[i], nums[i]);
+    }
+}
+
+void test_sort_with_dupes(void)
+{
+    int    nums[]     = { 2, 8, 3, 0, 9, 1, 4, 0, 8, 8, 15, 9 };
+    int    solution[] = { 0, 0, 1, 2, 3, 4, 8, 8, 8, 9, 9, 15 };
+    size_t len        = 12;
+
+    merge_sort(nums, len);
+
+    for (int i=0; i<len; ++i) {
+        TEST_ASSERT_EQUAL(solution[i], nums[i]);
     }
 }
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_CreateNode_handles_string_vals);
-    RUN_TEST(test_CreateNode_handles_float_vals);
-    RUN_TEST(test_CreateNode_handles_int_vals);
-    RUN_TEST(failing_test);
-    RUN_TEST(test_node_update);
+    RUN_TEST(test_sort);
+    RUN_TEST(test_sort_with_dupes);
     return UNITY_END();
 }
